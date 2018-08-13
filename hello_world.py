106e1d3b89ded9be4cb6359f0b551e32c7dc6a5b
hzgjl <hzgjl@tairanchina.com>
2018-08-13 18:13:21 +0800

    first commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: UTF-8 -*-
+hello = "hello world!"
+print(hello)
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, '等于', x, '*', n//x)
+            break
+    else:print(n, '是一个素数')
+        
+            
+
+# print hello.decode("utf-8").encode("gbk");
