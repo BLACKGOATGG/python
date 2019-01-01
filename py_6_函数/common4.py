3b0097c209d133c7dff13c029699fcf4cf3a3de8
hzgjl <hzgjl@tairanchian.com>
2019-01-01 16:52:55 +0800

    将函数存储在模块中;gitgnore

---
+++ py_6_函数/common4.py
@@ -0,0 +1,8 @@
+def fun10():
+    print("fun10") 
+
+def fun11():
+    print("fun11") 
+
+def fun12():
+    print("fun12") 
---
+++ py_6_函数/common5.py
@@ -0,0 +1,8 @@
+def fun13():
+    print("fun13") 
+
+def fun14():
+    print("fun14") 
+
+def fun15():
+    print("fun15") 
