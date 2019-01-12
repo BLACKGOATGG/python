90fc6ce6a9c21d20dfd621e35e7a8006be5b3fd9
hzgjl <hzgjl@tairanchian.com>
2019-01-12 17:57:37 +0800

    逐行读取

--- py_1_变量,字符串,输出/基本:去除字符串前后空格.py
+++ py_1_变量,字符串,输出/基本:去除字符串前后空格.py
@@ -1,7 +1,7 @@
 #======================================================
 favorite_language = " pyth on "
-favorite_language_rstrip = favorite_language.rstrip() 
-favorite_language_lstrip = favorite_language.lstrip() 
-favorite_language_strip = favorite_language.strip() 
+favorite_language_rstrip = favorite_language.rstrip()   # 尾部空格
+favorite_language_lstrip = favorite_language.lstrip()   # 头部空格
+favorite_language_strip = favorite_language.strip()     # 前后空格
 
 print(favorite_language_rstrip,favorite_language_lstrip,favorite_language_strip) 
